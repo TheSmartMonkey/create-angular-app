3f16a53858096dd2a46aa583d145e8f56b4cf07f
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2022-09-26 21:22:39 +0200

    fix navigation router

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  currentPage: string = 'home';
+export class NavbarComponent implements OnInit {
+  currentPage: string = '/home';
+  navigationSubscription: Subscription | undefined;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.navigationSubscription = this.router.events.subscribe((route) => {
+      if (route instanceof NavigationEnd) {
+        this.currentPage = route.url;
+      }
+    });
+  }
 
   navigate(page: string): void {
     this.currentPage = page;
+  }
+
+  ngOnDestroy(): void {
+    this.navigationSubscription?.unsubscribe();
   }
 }
